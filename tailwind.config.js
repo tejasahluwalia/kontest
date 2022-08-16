4d844c3b18c9fc2235fe9a423f4c36ad27ef04e8
Tejas Ahluwalia <tejasahluwalia@gmail.com>
2022-08-16 15:43:33 +0530

    add tailwind forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/forms")],
 };
