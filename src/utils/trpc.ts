70bdc6fa4f6409061204c6dd243f4f28a08948de
Tejas Ahluwalia <tejasahluwalia@gmail.com>
2022-08-16 15:42:00 +0530

    add trpc ssr support

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,9 +1,51 @@
 // src/utils/trpc.ts
-import type { AppRouter } from "../server/router";
-import { createReactQueryHooks } from "@trpc/react";
-import type { inferProcedureOutput, inferProcedureInput } from "@trpc/server";
+import { setupTRPC } from "@trpc/next";
+import type { inferProcedureInput, inferProcedureOutput } from "@trpc/server";
+import type { AppRouter } from "../server/trpc/router";
+import superjson from "superjson";
 
-export const trpc = createReactQueryHooks<AppRouter>();
+const getBaseUrl = () => {
+	if (typeof window !== "undefined") return ""; // browser should use relative url
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+	return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
+export const trpc = setupTRPC<AppRouter>({
+	config({ ctx }) {
+		if (typeof window !== "undefined") {
+			// during client requests
+			return {
+				transformer: superjson, // optional - adds superjson serialization
+				url: "/api/trpc",
+			};
+		}
+		// during SSR below
+
+		// // optional: use SSG-caching for each rendered page (see caching section for more details)
+		// const ONE_DAY_SECONDS = 60 * 60 * 24;
+		// ctx?.res?.setHeader(
+		// 	"Cache-Control",
+		// 	`s-maxage=1, stale-while-revalidate=${ONE_DAY_SECONDS}`
+		// );
+
+		// The server needs to know your app's full url
+		// On render.com you can use `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}/api/trpc`
+		const url = process.env.VERCEL_URL
+			? `https://${process.env.VERCEL_URL}/api/trpc`
+			: "http://localhost:3000/api/trpc";
+
+		return {
+			transformer: superjson, // optional - adds superjson serialization
+			url,
+			headers: {
+				// optional - inform server that it's an ssr request
+				"x-ssr": "1",
+			},
+		};
+	},
+	ssr: true,
+});
 
 /**
  * This is a helper method to infer the output of a query resolver
